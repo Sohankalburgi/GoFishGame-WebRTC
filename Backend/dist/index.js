fc45a4ef6121aeaa955d430762524df3ebf61b63
Sohankalburgi <sohankalburgi2004@gmail.com>
2024-11-13 20:27:31 +0530

    User Login and Register Backend

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -1,3 +1,61 @@
 "use strict";
-const hello = "hello";
-console.log("hello");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = require('./models/UserModel');
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+app.use(express.json());
+mongoose
+    .connect("mongodb://localhost:27017/GoFish")
+    .then(() => {
+    console.log("Mongo DB Connection is Established");
+})
+    .catch((error) => console.log(error));
+app.post('/register', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(400).json({ error: "username and password client error" });
+    }
+    if (yield User.findOne({ username })) {
+        return res.status(400).json({ error: "username already exist" });
+    }
+    const hashedPassword = yield bcrypt.hash(password, 14);
+    const user = new User({ username, password: hashedPassword });
+    yield user.save();
+    return res.status(200).json({ message: "User created" });
+}));
+app.post('/login', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { username, password } = req.body;
+    const user = yield User.findOne({ username });
+    if (!user) {
+        return res.status(400).json({ error: "user not found, Please Login" });
+    }
+    if (!(yield bcrypt.compare(password, user.password))) {
+        return res.status(400).json({ error: "password is wrong" });
+    }
+    return res.status(200).json({ message: "user authenticated" });
+}));
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.emit('connected', socket.id);
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+server.listen(3000, () => {
+    console.log('server running at http://localhost:3000');
+});
--- Backend/index.ts
+++ Backend/index.ts
@@ -1,2 +1,73 @@
-const hello = "hello";
-console.log("hello");
+import { Socket } from "socket.io";
+
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
+const User = require('./models/UserModel')
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+
+
+app.use(express.json());
+mongoose
+  .connect("mongodb://localhost:27017/GoFish")
+  .then(() => {
+    console.log("Mongo DB Connection is Established");
+  })
+  .catch((error:any) => console.log(error));
+
+app.post('/register',async (req:any,res:any)=>{
+  const {username, password} = req.body;
+
+  if(!username || !password){
+    return res.status(400).json({ error: "username and password client error" });
+  }
+
+  if(await  User.findOne({username})){
+    return res.status(400).json({ error: "username already exist" });
+  }
+
+  const hashedPassword:string =  await bcrypt.hash(password,14);
+
+  const user = new User({username,password :hashedPassword});
+  await user.save();
+
+  return res.status(200).json({message : "User created"})
+})
+
+
+app.post('/login', async(req:any, res:any)=>{
+  const {username,password} = req.body;
+
+  const user = await User.findOne({username});
+  if(!user){
+    return res.status(400).json({error:"user not found, Please Login"});
+  }
+
+  if(!await bcrypt.compare(password,user.password)){
+    return res.status(400).json({error : "password is wrong"});
+  }
+  return res.status(200).json({message : "user authenticated"});
+})
+
+
+io.on('connection', (socket:Socket) => {
+  console.log('a user connected');
+  
+  socket.emit('connected', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
